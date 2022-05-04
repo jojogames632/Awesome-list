e2cb4d6b070390fec77412189bc0d2ccc7a7cdba
jojogames632 <jojogames@free.fr>
2022-05-04 11:06:38 +0200

    Add banner and features-list in home page + add planning page

---
+++ src/app/core/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'al-footer',
+  templateUrl: './footer.component.html',
+  styles: [
+
+  ]
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
